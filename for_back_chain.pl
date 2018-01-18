3aa5b849732e7c6ce472156509b0fce9990b4b16
Verntrax <pawel.witcher@wp.pl>
2018-01-18 14:31:51 +0100

    Add comments to inference engine

--- for_back_chain.pl
+++ for_back_chain.pl
@@ -1,7 +1,5 @@
-:- dynamic used/1.
-
-:- op(800,xfx,=>).
-:- op(300,xfy,and).
+:- op(800,xfx,=>).	%dodatkowy operator implikacji
+:- op(300,xfy,and).	%dodatkowy operator koniunkcji
 
 :- consult('rules.pl').
 :- consult('facts.pl').
@@ -9,23 +7,34 @@
 write_proved(X) :- write(X), write(" is proved"), nl.
 write_true(X) :- write(X), write(" is true"), nl.
 
-backward(X) :- backward(X,[]).
-backward(X) :- fact(X), nl, write_true(X).
-backward(X,Used) :- fact(X).
-backward(X,Used) :- rule(Y => X), \+member(Y => X,Used), add_elem(Used,Y => X,Newused), backward(Y,Newused), \+fact(X), assert(fact(X)), write_proved(X).
-backward(X and Y,Used) :- backward(X,Used), backward(Y,Used).
+%%%%%%%%%%%%%%%%%%%%%%   Wnioskowanie wstecz  %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+backward(X) :- backward(X,[]).							%inicjuje obliczenia
+backward(X) :- fact(X), nl, write_true(X).					%wypisuje wynik
 
-forward(X) :- (search -> true; fact(X), nl, write_true(X)).
-search :- new_fact(Y), !, assert(fact(Y)), write_proved(Y), search.
+backward(X,Used) :- fact(X).							%zwraca prawdę dla już udowodnionych
 
-new_fact(X) :- provable(_=>X), \+fact(X).
+backward(X,Used) :- rule(Y => X), 						%szuka reguły dowodzącej
+                    \+member(Y => X,Used), add_elem(Used,Y => X,Newused), 	%sprawdza i uzupełnia zbiór użytych reguł
+                    backward(Y,Newused), 					%próbuje dowieść hipotezę
+                    \+fact(X), assert(fact(X)), write_proved(X).		%dodaje nowy fakt, jeśli go nie było
 
-provable(X=>Y) :- rule(X=>Y), satisfied(X).
+backward(X and Y,Used) :- backward(X,Used), backward(Y,Used).			%rozbija koniunkcję
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-satisfied(X) :- fact(X).
-satisfied(X and Y) :- satisfied(X), satisfied(Y).
+%%%%%%%%%%%%%%%%%%%%%%  Wnisokowanie w przód  %%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+forward(X) :- (search -> true; fact(X), nl, write_true(X)).			%inicjuje obliczenia, wypisuje wynik
+search :- new_fact(Y), !, assert(fact(Y)), write_proved(Y), search.		%właściwa pętla dowodząca kolejne fakty
 
-add_elem(List,X,Newlist) :- bagof(Y,add_fun(Y,X,List),Newlist).
+new_fact(X) :- provable(_=>X), \+fact(X).					%szuka nowego faktu, który daje się dowieść
+
+provable(X=>Y) :- rule(X=>Y), satisfied(X).					%sprawdza możliwość dowiedzenia stwierdzenia
+
+satisfied(X) :- fact(X).							%
+satisfied(X and Y) :- satisfied(X), satisfied(Y).				%razem sprawdzają prawdziwość zbioru faktów połączonych koniunkcją
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%  Funkcja pomocnicza (dodaje element do listy)  %%%%%%%%%%%%%%
+add_elem(List,X,Newlist) :- bagof(Y,add_fun(Y,X,List),Newlist).			
 add_fun(Y,X,List) :- member(Y,List); Y=X.
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
